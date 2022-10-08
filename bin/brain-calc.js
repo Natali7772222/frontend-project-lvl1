9f414dda8103570d368f701acca8b14eef731a72
Наталья <natalya@MacBook-Pro-Natala.local>
2022-10-08 22:27:26 +0300

    changed brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,12 +1,44 @@
 #!/usr/bin/env node
 
 import {
-    greetings
+    question
+} from "readline-sync";
+
+import {
+    greetings,
+    newName
 
 } from '../src/index.js';
-import {
-    calc
-} from './brain-even.js';
 
 greetings()
+const randomNum = () => {
+    return Math.ceil(Math.random() * 10);
+}
+const count = 0;
+
+const calc = () => {
+    let num1 = randomNum();
+    let num2 = randomNum();
+    let res = num1 + num2;
+    if (count < 3) {
+        console.log(`\nWhat is the result of the expression?
+        ${num1} + ${num2}`)
+
+        const quest = question()
+
+        if (quest == res) {
+            console.log(`Correct!`)
+            // count = count + 1;
+            calc()
+        } else {
+            console.log(`${newName}, is wrong answer ;(. Correct answer was ${res}.
+
+            Let's try again!
+
+            `)
+        }
+    } else {
+        console.log(`Congratulation! ${newName}`);
+    }
+}
 calc()
