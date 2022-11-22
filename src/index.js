738682f7268855d0ad9910a7e5a046d9d0c75683
AG235HB <ag235hb@hotmail.com>
2022-11-23 01:39:50 +0300

    calc game works well

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import {
     question
 } from "readline-sync";
 
-import  { runGame} from 
-"./games/calc.js"
+import { runGame } from
+    "./games/calc.js"
 "./games/even.js"
 
 let name;
 
-const greetings=()=>{
+const greetings = () => {
     console.log('Welcome to the Brain Games!');
     name = question(`May I have your name? `)
     console.log(`Hello, ${name} !`);
@@ -18,19 +18,18 @@ const randomNum = () => {
     return Math.ceil(Math.random() * 10);
 }
 let count = 1;
-const responseCheck = () => {
+const responseCheck = (correctAnswer) => {
 
     const userAnswer = question()
     if (count >= 3) {
         console.log(`Congratulations, ${name}!`)
     } else {
-
-        if (userAnswer === correctAnswer) {
+        if (userAnswer == correctAnswer) {
 
             console.log(`Correct!`)
             count = count + 1;
             runGame()
-        } else if (userAnswer !== correctAnswer) {
+        } else if (userAnswer != correctAnswer) {
             console.log(`${userAnswer}, is wrong answer ;(. Correct answer was ${correctAnswer}.
     
                 Let's try again!
@@ -42,7 +41,7 @@ const responseCheck = () => {
 }
 
 
-export{
+export {
     greetings,
     responseCheck,
     randomNum,
