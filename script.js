7ad52c7b74080873947331488557be1403b1b6c2
Наталья <natalya@MacBook-Pro-Natala.local>
2022-12-23 22:10:11 +0300

    bads fix

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
+function normalize(a) {
+  console.log(a.name.toLowerCase());
+  console.log(a.description.toLowerCase());
+}
+const lesson = {
+  name: 'ДеструКТУРИЗАЦИЯ',
+  description: 'каК удивитЬ друзей',
+};
 
-function normalize (a){
-     console.log( a.name.toLowerCase())
-     console.log(a.description.toLowerCase())
-    }
-    const lesson = {
-        name: 'ДеструКТУРИЗАЦИЯ',
-        description: 'каК удивитЬ друзей',
-      };
-  
-normalize(lesson)
-  
-  
+normalize(lesson);
